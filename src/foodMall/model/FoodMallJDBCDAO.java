cebb83358105b6f2ed9829d78979859428c442f2
User <User@DESKTOP-C3HHOV0>
2019-02-12 23:53:50 +0800

    FoodMallJDBCDAO完成insert, update 待改進用其他方式輸入CLOB

--- src/foodMall/model/FoodMallJDBCDAO.java
+++ src/foodMall/model/FoodMallJDBCDAO.java
@@ -1,13 +1,16 @@
 package foodMall.model;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -45,22 +48,22 @@ public class FoodMallJDBCDAO implements FoodMallDAO_interface {
 
 			pstmt.executeUpdate();
 		} catch (ClassNotFoundException e) {
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
 		} catch (SQLException e) {
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
 		} finally {
 			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
-					System.out.println(e.getStackTrace());
+					e.printStackTrace();
 				}
 			}
 			if (con != null) {
 				try {
 					con.close();
 				} catch (SQLException e) {
-					System.out.println(e.getStackTrace());
+					e.printStackTrace();
 				}
 			}
 		}
@@ -90,22 +93,22 @@ public class FoodMallJDBCDAO implements FoodMallDAO_interface {
 
 			pstmt.executeUpdate();
 		} catch (ClassNotFoundException e) {
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
 		} catch (SQLException e) {
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
 		} finally {
 			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
-					System.out.println(e.getStackTrace());
+					e.printStackTrace();
 				}
 			}
 			if (con != null) {
 				try {
 					con.close();
 				} catch (SQLException e) {
-					System.out.println(e.getStackTrace());
+					e.printStackTrace();
 				}
 			}
 		}
@@ -145,14 +148,14 @@ public class FoodMallJDBCDAO implements FoodMallDAO_interface {
 				try {
 					pstmt.close();
 				} catch (SQLException se) {
-					se.printStackTrace(System.err);
+					se.printStackTrace();
 				}
 			}
 			if (con != null) {
 				try {
 					con.close();
 				} catch (Exception e) {
-					e.printStackTrace(System.err);
+					e.printStackTrace();
 				}
 			}
 		}
@@ -160,7 +163,43 @@ public class FoodMallJDBCDAO implements FoodMallDAO_interface {
 
 	@Override
 	public FoodMallVO findByPrimaryKey(String foodSupId, String foodId) {
-
+		FoodMallVO foodMallVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		
+			try {
+				Class.forName(DRIVER);
+				con = DriverManager.getConnection(URL, USER, PASSWORD);
+				pstmt = con.prepareStatement(GET_ONE_STMT);
+				
+				pstmt.setString(1, foodSupId);
+				pstmt.setString(2, foodId);
+				
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					foodMallVO = new FoodMallVO();
+					foodMallVO.setFoodSupId(rs.getString(1));
+					foodMallVO.setFoodId(rs.getString(2));
+					foodMallVO.setFoodMName(rs.getString(3));
+					foodMallVO.setFoodMStatus(rs.getString(4));
+					foodMallVO.setFoodMPrice(rs.getInt(5));
+					foodMallVO.setFoodMUnit(rs.getString(6));
+					foodMallVO.setFoodMPlace(rs.getString(7));
+					
+//					foodMallVO.setFoodMPic();
+					
+				}
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			
+		
 		return null;
 	}
 
@@ -171,25 +210,56 @@ public class FoodMallJDBCDAO implements FoodMallDAO_interface {
 	}
 	
 	public static void main(String[] args) {
+		FoodMallJDBCDAO fMDao = new FoodMallJDBCDAO();
+		
 		// 新增
 		FoodMallVO foodMallVO = new FoodMallVO();
-		foodMallVO.setFoodSupId(foodSupId);
+		foodMallVO.setFoodSupId("C00005");
+		foodMallVO.setFoodId("F00001");
+		foodMallVO.setFoodMName("五穀米");
+		foodMallVO.setFoodMStatus("p2");
+		foodMallVO.setFoodMPrice(300);
+		foodMallVO.setFoodMUnit("1kg");
+		foodMallVO.setFoodMPlace("台灣");
+		foodMallVO.setFoodMPic(fMDao.getPictureByteArray("P:/pic/澳洲牛頰肉.png"));
+		foodMallVO.setFoodMResume("飽滿");
+		foodMallVO.setFoodMRate(5);
+		fMDao.insert(foodMallVO);
+		// 更新
+//		FoodMallVO foodMallVO = new FoodMallVO();
+//		foodMallVO.setFoodSupId("C00005");
+//		foodMallVO.setFoodId("F00001");
+//		foodMallVO.setFoodMName("馬玉山五穀米");
+//		foodMallVO.setFoodMStatus("p4");
+//		foodMallVO.setFoodMPrice(250);
+//		foodMallVO.setFoodMUnit("0.6kg");
+//		foodMallVO.setFoodMPlace("馬玉山");
+//		foodMallVO.setFoodMPic(fMDao.getPictureByteArray("P:/pic/馬玉山五穀米.jpg"));
+//		foodMallVO.setFoodMResume("煮起來又香又好吃, 對身體的健康又很有幫助");
+//		foodMallVO.setFoodMRate(4);
+//		fMDao.update(foodMallVO);
+		// 刪除
+//		fMDao.delete("C00005", "F00001");
 	}
 	
 	private byte[] getPictureByteArray(String filePath) {
 		File pic = new File(filePath);
 		FileInputStream fis = null;
 		ByteArrayOutputStream baos = null;
-		
+		byte[] buffer =null;
 		try {
 			fis = new FileInputStream(pic);
 			baos = new ByteArrayOutputStream();
-			byte[] buffer = new byte[fis.available()];
-			int i;
+			buffer = new byte[fis.available()];
+			// 如果我用
+//			byte[] buffer = new byte[fis.available()];
+//			int i;			
+//			while (( i = fis.read(buffer)) != -1) {
+//				baos.write(buffer, 0, i);
+//			}
 			
-			while (( i = fis.read(buffer)) != -1) {
-				baos.write(buffer, 0, i);
-			}
+			fis.read(buffer);
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -210,6 +280,22 @@ public class FoodMallJDBCDAO implements FoodMallDAO_interface {
 				}
 			}
 			
+		}
+		return buffer;
+//		return baos.toByteArray();
+	}
+	
+	private byte[] getPictureByteArray(InputStream in) {
+		BufferedInputStream bIn = new BufferedInputStream(in);
+		ByteArrayOutputStream baos = null;
+		byte[] buf = new byte[4 * 1024];
+		int len;
+		try {
+			while((len = bIn.read(buf)) != -1) {
+				baos.write(buf);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return baos.toByteArray();
 	}
