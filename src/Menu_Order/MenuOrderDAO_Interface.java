96b8651fbf73c2fdfe822f3048d412f0f96c1914
Ito <Ito@MOS-ITO>
2019-02-13 23:14:45 +0800

    fix space

--- src/Menu_Order/MenuOrderDAO_Interface.java
+++ src/Menu_Order/MenuOrderDAO_Interface.java
@@ -3,13 +3,12 @@ package Menu_Order;
 import java.util.List;
 
 public interface MenuOrderDAO_Interface {
-
+	
 	public void insert(MenuOrderVO menuOdVO);
     public void update(MenuOrderVO menuOdVO);
     public void delete(String menuOdId);
     public MenuOrderVO findByPrimaryKey(String menuOdId);
-    public List<MenuOrderVO> getAll();
-    
+    public List<MenuOrderVO> getAll();    
 //  萬用複合查詢(傳入參數型態Map)(回傳 List)
 //  public List<ChefVO> getAll(Map<String, String[]> map);
 }
