0d11ea3f1ea5f719233047c28a8fbf36e9cbced7
angela <wangchia6688@gmail.co>
2019-02-18 12:37:40 +0800

    FestMenu_Interface.java及FestMenuVO.java

---
+++ src/com/festMenu/model/FestMenu_Interface.java
@@ -0,0 +1,11 @@
+package com.festMenu.model;
+
+import java.util.List;
+
+public interface FestMenu_Interface {
+	void insert(FestMenuVO festMenuVO);
+	void update(FestMenuVO festMenuVO);
+	void delete(String fest_m_ID);
+	FestMenuVO findByPrimaryKey(String fest_m_ID);
+	List<FestMenuVO> getAll();
+}
